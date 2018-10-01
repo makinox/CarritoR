76af4baeee77b2c0a5a1fad1fea15b7680b72eb8
Jesus bossa <jesusbossa@protonmail.com>
2018-10-01 16:38:39 -0500

    detail initialized

--- src/dist/section/components/detailComponent.js
+++ src/dist/section/components/detailComponent.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default () => (
-  <div>
-    hola detalle
+import './detailComponent.css'
+
+export default (props) => (
+  <div className="detail-component">
+    {console.log(props.selected)}
+    {props.selected.map((el, i) => (
+      <div className="detail-component-items" key={i}>
+          <p><strong>Producto: </strong>{el.product}</p>
+          <p><strong>Unidades: </strong>{el.unity}</p>
+          <p><strong>Total: </strong>{el.unity * el.cost}</p>
+      </div>
+    ))}
+    <div style={{justifySelf: 'center'}}>
+      <p><strong>Total: </strong>{props.total}</p>
+    </div>
+    <div className="detail-component-footer">
+      <a>Comprar</a>
+      <a>Cancelar</a>
+    </div>
   </div>
 )
--- src/dist/section/container/detail.js
+++ src/dist/section/container/detail.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Detail from '../components/detailComponent'
 
 class App extends React.Component {
   render(){
-    return <Detail />
+    return <Detail selected={this.props.selected} total={this.props.total} />
   }
 }
 
-export default App
+function mapStateToProps(state){
+  return{
+    selected: state.selected,
+    total: state.total
+  }
+}
+
+export default connect(mapStateToProps)(App)
