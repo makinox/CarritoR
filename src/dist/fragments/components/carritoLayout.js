36ae2b33510aad12f80f04c22e78f23652cf04e1
Jesus bossa <jesusbossa@protonmail.com>
2018-09-30 19:48:31 -0500

    Carrito layout initialized

--- src/dist/fragments/components/carritoLayout.js
+++ src/dist/fragments/components/carritoLayout.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import './carritoLayout.css'
 
 export default () => (
-  <div>
-    carrito
-  </div>
+  <section>
+    <div className="carrito">
+      <div className="carrito-header">
+        <p><strong>Total productos: </strong>2</p>
+      </div>
+      <div className="carrito-body">
+        <div>
+          <p>producto #1</p>
+          <a >❌</a>
+        </div>
+        <div>
+          <p>producto #2</p>
+          <a >❌</a>
+        </div>
+        <div>
+          <p>producto #3</p>
+          <a >❌</a>
+        </div>
+        <div>
+          <p>producto #4</p>
+          <a >❌</a>
+        </div>
+      </div>
+      <div className="carrito-footer">
+        <a>Comprar</a>
+        <a>Detalle</a>
+      </div>
+    </div>
+  </section>
 )
